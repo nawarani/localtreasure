c6bda91b434b7b12501dc4eecdf64cd2350039a1
Anika Nawar <nawarani47@gmail.com>
2025-07-07 00:26:14 -0400

    changed db setup for render inside connections file and deleted the extra db schema files

--- backend/db/schema/04_messaging.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS messaging CASCADE;
-
-CREATE TABLE messaging (
-  msg_id SERIAL PRIMARY KEY NOT NULL,
-  sender_id INTEGER REFERENCES users(id),
-  receiver_id INTEGER REFERENCES users(id),
-  content TEXT,
-  sendtime TIMESTAMP
-);
